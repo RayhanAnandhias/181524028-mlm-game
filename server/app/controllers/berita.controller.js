63b888538f864e7bb8aa2d31655298274fb8707a
RayhanAnandhias <48201126+RayhanAnandhias@users.noreply.github.com>
2020-10-17 13:15:48 +0700

    fix typo

--- server/app/controllers/berita.controller.js
+++ server/app/controllers/berita.controller.js
@@ -1,136 +1,145 @@
 const db = require("../models");
 const Berita = db.berita;
-const Op = db.Sequlize.Op;
+const Op = db.Sequelize.Op;
 
 // Create and Save berita
 exports.create = (req, res) => {
-    // Validate request
-    if(!req.body.judul_berita) {
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
-        return;
-    }
+  // Validate request
+  if (!req.body.judul_berita) {
+    res.status(400).send({
+      message: "Content can not be empty!",
+    });
+    return;
+  }
 
-    // Create berita
-    const berita = {
-        judul_berita: req.body.judul_berita,
-        konten: req.body.konten,
-        kategori: req.body.kategori
-    };
+  // Create berita
+  const berita = {
+    judul_berita: req.body.judul_berita,
+    konten: req.body.konten,
+    kategori: req.body.kategori,
+  };
 
-    // save berita in the database
-    Berita.create(berita).then(data => {
-        res.send(data);
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while creating berita."
-        });
+  // save berita in the database
+  Berita.create(berita)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating berita.",
+      });
     });
 };
 
 exports.findAll = (req, res) => {
-    const judul_berita = req.query.judul_berita;
-    var condition = judul_berita ? { judul_berita: { [Op.iLike]: `%${judul_berita}%` } } : null;
-  
-    Berita.findAll({ where: condition })
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving berita."
-        });
+  const judul_berita = req.query.judul_berita;
+  var condition = judul_berita
+    ? { judul_berita: { [Op.iLike]: `%${judul_berita}%` } }
+    : null;
+
+  Berita.findAll({ where: condition })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving berita.",
       });
+    });
 };
 
 exports.findOne = (req, res) => {
-    const id = req.params.id;
-  
-    Berita.findByPk(id)
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Error retrieving berita with id=" + id
-        });
+  const id = req.params.id;
+
+  Berita.findByPk(id)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error retrieving berita with id=" + id,
       });
+    });
 };
 
 exports.update = (req, res) => {
-    const id = req.params.id;
-  
-    Berita.update(req.body, {
-      where: { id: id }
-    })
-      .then(num => {
-        if (num == 1) {
-          res.send({
-            message: "berita was updated successfully."
-          });
-        } else {
-          res.send({
-            message: `Cannot update Tutorial with id=${id}. Maybe berita was not found or req.body is empty!`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Error updating Tutorial with id=" + id
+  const id = req.params.id;
+
+  Berita.update(req.body, {
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "berita was updated successfully.",
         });
+      } else {
+        res.send({
+          message: `Cannot update Tutorial with id=${id}. Maybe berita was not found or req.body is empty!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating Tutorial with id=" + id,
       });
+    });
 };
 
 exports.delete = (req, res) => {
-    const id = req.params.id;
-  
-    Berita.destroy({
-      where: { id: id }
-    })
-      .then(num => {
-        if (num == 1) {
-          res.send({
-            message: "Berita was deleted successfully!"
-          });
-        } else {
-          res.send({
-            message: `Cannot delete berita with id=${id}. Maybe berita was not found!`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Could not delete berita with id=" + id
+  const id = req.params.id;
+
+  Berita.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Berita was deleted successfully!",
         });
+      } else {
+        res.send({
+          message: `Cannot delete berita with id=${id}. Maybe berita was not found!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete berita with id=" + id,
       });
+    });
 };
 
 exports.deleteAll = (req, res) => {
-    Berita.destroy({
-      where: {},
-      truncate: false
+  Berita.destroy({
+    where: {},
+    truncate: false,
+  })
+    .then((nums) => {
+      res.send({ message: `${nums} all berita were deleted successfully!` });
     })
-      .then(nums => {
-        res.send({ message: `${nums} all berita were deleted successfully!` });
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while removing all berita."
-        });
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all berita.",
       });
+    });
 };
 
 exports.findAllByCategory = (req, res) => {
-    const kategori = req.query.kategori;
-    var condition = kategori ? { kategori: { [Op.iLike]: `%${kategori}%` } } : null;
-    Berita.findAll({ where: condition }).then(data => {
-        res.send(data);
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while retrieving berita by category."
-        })
+  const kategori = req.query.kategori;
+  var condition = kategori
+    ? { kategori: { [Op.iLike]: `%${kategori}%` } }
+    : null;
+  Berita.findAll({ where: condition })
+    .then((data) => {
+      res.send(data);
     })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message ||
+          "Some error occurred while retrieving berita by category.",
+      });
+    });
 };
--- server/app/models/index.js
+++ server/app/models/index.js
@@ -2,20 +2,20 @@ const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    operatorsAliases: false,
-    pool: {
-        max: dbConfig.pool.max,
-        min: dbConfig.pool.min,
-        acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle
-    }
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
 });
 
 const db = {};
 
-db.Sequlize = Sequelize;
+db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.berita = require("./berita.model.js")(sequelize, Sequelize);
